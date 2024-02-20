d160afae41f17a27308a47197fabbcdcdfa0d2a4
高桥琳介 <18574945291@163.com>
2024-02-20 21:16:51 +0800

    init project + 1

---
+++ backend/internal/web/user.go
@@ -0,0 +1,14 @@
+//@Author: wulinlin
+//@Description:
+//@File:  user
+//@Version: 1.0.0
+//@Date: 2024/02/20 21:15
+
+package web
+
+type UserHandler struct {
+}
+
+func (u *UserHandler) SignUp(ctx *gin.Context) {
+
+}
