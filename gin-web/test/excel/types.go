42e9de04ce7f5be6960801f742512195258af127
wulinlin <18574945291@163.com>
2024-03-19 21:52:53 +0800

    算了算了

---
+++ gin-web/test/excel/types.go
@@ -0,0 +1,12 @@
+package excel
+
+// @Description
+// @Author 代码小学生王木木
+// @Date 2024-03-19 21:50
+
+type Company struct {
+	CompanyName string
+	TaxCode     string
+	Province    string
+	City        string
+}
