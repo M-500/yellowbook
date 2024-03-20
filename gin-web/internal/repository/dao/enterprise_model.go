e571e75fe964b320c27fbfb8d746bd4032b9b82f
高桥琳介 <18574945291@163.com>
2024-03-20 09:40:41 +0800

    妈的 mac敲代码就是垃圾

--- gin-web/internal/repository/dao/enterprise_model.go
+++ gin-web/internal/repository/dao/enterprise_model.go
@@ -11,41 +11,41 @@ import (
 
 type Enterprise struct {
 	gorm.Model
-	EnterpriseName            string     `gorm:"type:varchar(200);comment:企业名称" json:"enterprise_name"`
-	RegistrationStatus        string     `gorm:"type:varchar(32);comment:登记状态" json:"registration_status"`
-	LegalRepresentative       string     `gorm:"type:varchar(64);comment:法定代表人" json:"legal_representative"`
-	RegisteredCapital         string     `gorm:"type:varchar(255);comment:注册资本" json:"registered_capital"`
-	RegDate                   *time.Time `gorm:"comment:成立日期" json:"reg_date"`
-	SocialCode                string     `gorm:"type:varchar(32);comment:统一社会信用代码;index" json:"social_code"`
-	EnterpriseRegAddr         string     `gorm:"type:varchar(255);comment:企业注册地址" json:"enterprise_reg_addr"`
-	Phone                     string     `gorm:"type:varchar(32);comment:电话" json:"phone"`
-	MorePhone                 string     `gorm:"type:varchar(255);comment:更多电话" json:"more_phone"`
-	Email                     string     `gorm:"type:varchar(255);comment:邮箱" json:"email"`
-	MoreEmail                 string     `gorm:"type:varchar(255);comment:更多邮箱" json:"more_email"`
-	Province                  string     `gorm:"type:varchar(255);comment:所属省份" json:"province"`
-	City                      string     `gorm:"type:varchar(255);comment:所属城市" json:"city"`
-	District                  string     `gorm:"type:varchar(255);comment:所属区县" json:"district"`
-	TaxNumber                 string     `gorm:"type:varchar(32);comment:纳税人识别号" json:"tax_number"`
-	RegNumber                 string     `gorm:"type:varchar(255);comment:注册号" json:"reg_number"`
-	OrgCode                   string     `gorm:"type:varchar(64);comment:组织机构代码" json:"org_code"`
-	InsuredNumber             int        `gorm:"comment:参保人数" json:"insured_number"`
-	InsuredNumberAnnualReport string     `gorm:"type:varchar(255);comment:参保人数所属年报" json:"insured_number_annual_report"`
-	EnterpriseType            string     `gorm:"type:varchar(255);comment:企业(机构)类型" json:"enterprise_type"`
-	EnterpriseSize            string     `gorm:"type:varchar(255);comment:企业规模" json:"enterprise_size"`
-	ApprovalDate              *time.Time `gorm:"comment:核准日期" json:"approval_date"`
-	BusinessTerm              string     `gorm:"type:varchar(255);comment:营业期限" json:"business_term"`
-	GBIndustryCategory        string     `gorm:"type:varchar(255);comment:国标行业门类" json:"gb_industry_category"`
-	GBIndustryMajorCategory   string     `gorm:"type:varchar(255);comment:国标行业大类" json:"gb_industry_major_category"`
-	GBIndustryMediumCategory  string     `gorm:"type:varchar(255);comment:国标行业中类" json:"gb_industry_medium_category"`
-	GBIndustrySmallCategory   string     `gorm:"type:varchar(255);comment:国标行业小类" json:"gb_industry_small_category"`
-	FormerName                string     `gorm:"type:varchar(255);comment:曾用名" json:"former_name"`
-	EnglishName               string     `gorm:"type:varchar(255);comment:英文名" json:"english_name"`
-	OfficialWebsite           string     `gorm:"type:varchar(255);comment:官网网址" json:"official_website"`
-	CommunicationAddress      string     `gorm:"type:varchar(255);comment:通信地址" json:"communication_address"`
-	EnterpriseProfile         string     `gorm:"type:text;comment:企业简介" json:"enterprise_profile"`
-	BusinessScope             string     `gorm:"type:text;comment:经营范围" json:"business_scope"`
-	QCCIndustryCategory       string     `gorm:"type:varchar(255);comment:企查查行业门类" json:"qcc_industry_category"`
-	QCCIndustryMajorCategory  string     `gorm:"type:varchar(255);comment:企查查行业大类" json:"qcc_industry_major_category"`
-	QCCIndustryMediumCategory string     `gorm:"type:varchar(255);comment:企查查行业中类" json:"qcc_industry_medium_category"`
-	QCCIndustrySmallCategory  string     `gorm:"type:varchar(255);comment:企查查行业小类" json:"qcc_industry_small_category"`
+	CompanyName               string     `gorm:"comment:企业名称;type:;size:;"`
+	RegStatus                 string     `gorm:"comment:登记状态;type:;size:;"`
+	LegalBoss                 string     `gorm:"comment:;type:;size:;"`
+	RegCapital                string     `gorm:"comment:;type:;size:;"`
+	RegDate                   *time.Time `gorm:"comment:;type:;size:;"`
+	SocialCode                string     `gorm:"comment:;type:;size:;"`
+	CompanyRegAddr            string     `gorm:"comment:;type:;size:;"`
+	Phone                     string     `gorm:"comment:;type:;size:;"`
+	MorePhone                 string     `gorm:"comment:;type:;size:;"`
+	Email                     string     `gorm:"comment:;type:;size:;"`
+	MoreEmail                 string     `gorm:"comment:;type:;size:;"`
+	Province                  string     `gorm:"comment:;type:;size:;"`
+	City                      string     `gorm:"comment:;type:;size:;"`
+	District                  string     `gorm:"comment:;type:;size:;"`
+	TaxNumber                 string     `gorm:"comment:;type:;size:;"`
+	RegNumber                 string     `gorm:"comment:;type:;size:;"`
+	OrgCode                   string     `gorm:"comment:;type:;size:;"`
+	InsuredNumber             int        `gorm:"comment:;type:;size:;"`
+	InsuredNumberReport       string     `gorm:"comment:;type:;size:;"`
+	CompanyType               string     `gorm:"comment:;type:;size:;"`
+	CompanySize               string     `gorm:"comment:;type:;size:;"`
+	ApprovalDate              *time.Time `gorm:"comment:;type:;size:;"`
+	BusinessTerm              string     `gorm:"comment:;type:;size:;"`
+	GBIndustryCategory        string     `gorm:"comment:;type:;size:;"`
+	GBIndustryMajorCategory   string     `gorm:"comment:;type:;size:;"`
+	GBIndustryMediumCategory  string     `gorm:"comment:;type:;size:;"`
+	GBIndustrySmallCategory   string     `gorm:"comment:;type:;size:;"`
+	FormerName                string     `gorm:"comment:;type:;size:;"`
+	EnglishName               string     `gorm:"comment:;type:;size:;"`
+	OfficialWebsite           string     `gorm:"comment:;type:;size:;"`
+	CommunicationAdd          string     `gorm:"comment:;type:;size:;"`
+	CompanyProfile            string     `gorm:"comment:;type:;size:;"`
+	BusinessScope             string     `gorm:"comment:;type:;size:;"`
+	QCCIndustryCategory       string     `gorm:"comment:;type:;size:;"`
+	QCCIndustryMajorCategory  string     `gorm:"comment:;type:;size:;"`
+	QCCIndustryMediumCategory string     `gorm:"comment:;type:;size:;"`
+	QCCIndustrySmallCategory  string     `gorm:"comment:;type:;size:;"`
 }
